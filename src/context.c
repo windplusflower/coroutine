5b2363cdb09637a0fa36e8a86b9b77eb6ba72977
windflower <windflower_weng@qq.com>
2024-12-05 16:28:54 +0800

    feat:完成协程切换的汇编实现（但还未接入项目）

---
+++ src/context.c
@@ -0,0 +1,20 @@
+#include "context.h"
+
+#include <string.h>
+
+void make_context(Context* context, void (*func)(void*), void* arg) {
+    //计算栈底位置，令sp指向栈底
+    char* sp = context->ss_sp + context->ss_size - sizeof(void*);
+    sp = (char*)((unsigned long)sp & -16L);
+
+    //存放返回地址，ret指令会跳转到当前sp所指地址
+    void** ret_addr = (void**)sp;
+    *ret_addr = (void*)func;
+
+    //初始化寄存器
+    memset(context->regs, 0, sizeof(context->regs));
+    context->regs[RSP] = sp;
+    context->regs[RDI] = arg;
+
+    return;
+}
--- src/context.h
+++ src/context.h
@@ -1,8 +1,30 @@
 #ifndef CONTEXT_H
 #define CONTEXT_H
 
+#include <stddef.h>
+typedef enum {
+    RAX = 0,
+    RBX = 1,
+    RCX = 2,
+    RDX = 3,
+    RSI = 4,  //第二个参数
+    RDI = 5,  //第一个参数
+    RBP = 6,  //栈帧
+    RSP = 7,  //栈指针
+    R8 = 8,
+    R9 = 9,
+    R10 = 10,
+    R11 = 11,
+    R12 = 12,
+    R13 = 13,
+    R14 = 14,
+    R15 = 15,
+} Reg;
 typedef struct Context {
-    int regs[16];
+    void* regs[16];
+    char* ss_sp;
+    size_t ss_size;
 } Context;
 
+void make_context(Context* co, void (*func)(void*), void* arg);
 #endif
