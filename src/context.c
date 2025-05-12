0f2fba9206d1f36081a3c2a21213ec565bb7b334
windflower <windflower_weng@qq.com>
2025-05-12 12:12:03 +0800

    fix:修改rflags的默认值为0x202

--- src/context.c
+++ src/context.c
@@ -26,14 +26,14 @@
 #include "log.h"
 
 //生成上下文
-void make_context(Context* context, void (*func)()) {
+void make_context(Context *context, void (*func)()) {
     //计算栈底位置，令sp指向栈底
-    char* sp = context->ss_sp + context->ss_size - sizeof(void*);
+    char *sp = context->ss_sp + context->ss_size - sizeof(void *);
     // x86_64要求sp对16字节对齐
-    sp = (char*)((unsigned long)sp & -16L);
+    sp = (char *)((unsigned long)sp & -16L);
     //存放返回地址，ret指令会跳转到当前sp所指地址
-    void** ret_addr = (void**)sp;
-    *ret_addr = (void*)func;
+    void **ret_addr = (void **)sp;
+    *ret_addr = (void *)func;
 
     //初始化通用寄存器
     memset(context->regs, 0, sizeof(context->regs));
@@ -42,6 +42,6 @@ void make_context(Context* context, void (*func)()) {
     //初始化栈指针
     context->regs[RSP] = sp;
     //初始化标志寄存器
-    context->rflags = (void*)2;
+    context->rflags = (void *)0x202;
     return;
 }
